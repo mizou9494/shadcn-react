2594a2e058030a326446f7a00e89b3e8021c3f60
mizou9494 <betarmizou@gmail.com>
2025-01-15 21:48:05 +0100

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Card } from "./components/ui/Card";
+import './index.css';
 function App() {
   return (
     <div className="App">
+      <h2 className="text-3xl font-bold underline">Card</h2>
       <Card />
     </div>
   );
